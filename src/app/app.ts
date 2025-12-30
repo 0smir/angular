aaf95601e9366f2214f0ffb5b50aa3d1397a28bf
o.smirnova <o.v.smirnova1987@gmail.com>
2025-12-30 15:20:13 +0200

    add main structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { UserComponent} from './components/user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,17 @@ import { UserComponent} from './components/user/user';
 })
 export class App {
   protected readonly title = signal('my-first-ang-app');
+  users = DUMMY_USERS;
+  
+ 
+  toggleContent() {
+    console.log('!!!!!');
+     let content = document.getElementsByClassName('content')[0] as HTMLElement;
+    if(content.classList.contains('open')){
+      content.classList.remove('open');
+    }else {
+      content.classList.add('open');
+    }
+
+  }
 }
